4a6972686b5bc0269a121d18fc9e6cb8f4a9862d
yamato0211 <kumayama0211@gmail.com>
2025-03-15 21:15:48 +0900

    feat: introduce infracost

--- infra/environments/dev/terraform.tf
+++ infra/environments/dev/terraform.tf
@@ -18,3 +18,16 @@ terraform {
 provider "aws" {
   region  = "ap-northeast-1"
 }
+
+resource "aws_lambda_function" "my_hello_world" {
+  runtime = "nodejs12.x"
+  handler = "exports.test"
+  image_uri = "test"
+  function_name = "test"
+  role = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
+
+  memory_size = 512
+  tags = {
+    Environment = "dev"
+  }
+}
